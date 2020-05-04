d17e63af50ff2ad7cd4a339391deeae2f5e80409
Nelio Alves <acenelio@gmail.com>
2020-05-04 18:34:34 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string sexo;
+    int cervejas, refri, espetos;
+
+    cout << "Sexo: ";
+    cin >> sexo;
+    cout << "Quantidade de cervejas: ";
+    cin >> cervejas;
+    cout << "Quantidade de regrigerantes: ";
+    cin >> refri;
+    cout << "Quantidade de espetinhos: ";
+    cin >> espetos;
+
+
     return 0;
 }
